202a07509bb5f36e06a5479f04f95c9e40589e75
Florencia <florcaminosgarcia@gmail.com>
2025-12-13 00:06:50 -0300

    Update: Ejercicios JavasCript Conversion de unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,7 +1,40 @@
 /**
- * Descripción
- * @method Nombre de la función
- * @param Parámetro A
- * @param Parámetro B
+ * Conversión de unidades, de metros, yardas, pies y pulgadas
+ * @method cambiarUnidades
+ * @param {string} id - El id de los inputs de metros, yardas, pies o pulgadas
+ * @param {number} valor - El valor de los inputs de metros, yardas, pies o pulgadas
  * @return Valor que retorna
  */
+function cambiarUnidades(id, valor) {
+    if (isNaN(valor)){
+        alert("Se ingreso un valor invalido");
+        document.lasUnidades.unid_metro.value = "";
+        document.lasUnidades.unid_pulgada.value = "";
+        document.lasUnidades.unid_pie.value = "";
+        document.lasUnidades.unid_yarda.value = "";
+    }
+    else if (id==="metro")
+    {
+        document.lasUnidades.unid_pulgada.value = 39.3701*valor;
+        document.lasUnidades.unid_pie.value = 3.28084*valor;
+        document.lasUnidades.unid_yarda.value = 1.09361*valor;
+    }
+    else if (id==="pulgada")
+    {
+        document.lasUnidades.unid_metro.value = 0.0254*valor;
+        document.lasUnidades.unid_pie.value = 0.083333*valor;
+        document.lasUnidades.unid_yarda.value = 0.027778*valor;
+    }
+    else if (id==="pie")
+    {
+        document.lasUnidades.unid_pulgada.value = 12*valor;
+        document.lasUnidades.unid_metro.value = 0.3048*valor;
+        document.lasUnidades.unid_yarda.value = 0.3333*valor;
+    }
+    else if (id==="yarda")
+    {
+        document.lasUnidades.unid_pulgada.value = 36*valor;
+        document.lasUnidades.unid_pie.value = 3*valor;
+        document.lasUnidades.unid_metro.value = 0.9144*valor;
+    }
+}
