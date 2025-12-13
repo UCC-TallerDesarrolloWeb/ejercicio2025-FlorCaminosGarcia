d36fcf1897b2ff5ca660c6e323bcf0656b47bf9a
Florencia <florcaminosgarcia@gmail.com>
2025-12-13 00:22:34 -0300

    Update: Ejercicios JavasCript _ grados-radianes

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -38,3 +38,18 @@ function cambiarUnidades(id, valor) {
         document.lasUnidades.unid_metro.value = 0.9144*valor;
     }
 }
+
+function convertirGR(id){
+    let grad, rad;
+    if(id==="grados"){
+        grad = document.getElementById("grados").value;
+        rad = (grad*Math.PI)/180;
+    }
+    else if(id==="radianes")
+    {
+        rad = document.getElementById("radianes").value;
+        grad = (rad*180)/Math.PI
+    }
+    document.getElementById("grados").value = grad;
+    document.getElementById("radianes").value = rad;
+}
